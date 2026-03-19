365f456b7106e1246a7210aec9fe14e64cb0b266
llaradearaujo <laradearaujo300@gmail.com>
2026-03-18 21:38:18 -0300

    LEETCODE ANAGRAM CONCLUÍDO

---
+++ java/Anagram.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] ar = s.toCharArray();
+        char[] arr = t.toCharArray();
+
+        Arrays.sort(ar);
+        Arrays.sort(arr);
+
+        if (Arrays.equals(ar, arr)) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
